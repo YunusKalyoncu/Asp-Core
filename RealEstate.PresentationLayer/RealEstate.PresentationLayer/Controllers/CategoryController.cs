f50c90416349611eac8b58ac7828277278e077ef
Yunus Kalyoncu <95497265+YunusKalyoncu@users.noreply.github.com>
2022-11-03 00:06:41 +0300

    3.11.2022
    Her güncellemede tarih değişecek

---
+++ RealEstate.PresentationLayer/RealEstate.PresentationLayer/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate.BusinessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstate.PresentationLayer.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ICategoryService _categoryservice;
+
+        public CategoryController(ICategoryService categoryservice)
+        {
+            _categoryservice = categoryservice;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _categoryservice.TGetList();
+            return View(values);
+        }
+    }
+}
