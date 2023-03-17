9119d995a3bae725116856dc5dd57b120d0ab724
Yunus Kalyoncu <95497265+YunusKalyoncu@users.noreply.github.com>
2023-03-17 09:22:51 +0300

    güncelleme

--- RealEstate.PresentationLayer/RealEstate.DataAccessLayer/Concrete/Context.cs
+++ RealEstate.PresentationLayer/RealEstate.DataAccessLayer/Concrete/Context.cs
@@ -13,7 +13,7 @@ namespace RealEstate.DataAccessLayer.Concrete
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=DESKTOP-0A6CNA1;initial catalog=DbCoreRealEstate; integrated security=true");
+            optionsBuilder.UseSqlServer("Server=YUNUS\\SQLEXPRESS;initial catalog=DbCoreRealEstate; integrated security=true");
         }
 
         public DbSet<Member> Members { get; set; }
